3a9186c60a3ad00a44aca48ea8a1942d9e388b93
Misha P <alakaganaguathoork@gmail.com>
2025-10-15 15:54:04 +0300

    Add Azure Pipelines scripts + .gitignore + .helmignore

---
+++ azure-pipelines/scripts/env/helm.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# This script automates AWS CLI installation/uninstallation based on provided argume nt `--action``.
+# Usage: helm.sh --action <install|uninstall>
+
+# ────────────────────────────────────────────────────────────────────────────────
+# Helpers
+# ────────────────────────────────────────────────────────────────────────────────
+
+color() {
+  # prints in bold magenta
+  printf "\e[1;35m%b\e[0m" "$1"
+}
+
+italic() {
+  printf "\e[3m%b\e[0m" "$1"
+}
+
+die() {
+  echo >&2 "❌ ${1}"
+  exit "${2:-1}"
+}
+
+# ────────────────────────────────────────────────────────────────────────────────
+# Defaults
+# ────────────────────────────────────────────────────────────────────────────────
+
+ACTION=""
+
+# ────────────────────────────────────────────────────────────────────────────────
+# Parse args
+# ────────────────────────────────────────────────────────────────────────────────
+
+parse_args() {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --action|-a)
+        ACTION="${2:-}"; shift 2 
+        ;;
+      -h|--help)
+        echo "$(color "Usage:") $(italic "$0 --action <install|uninstall>")"
+        exit 0 
+        ;;
+      *)
+        die "Unknown argument: $1"
+        ;;
+    esac
+  done
+
+  # validate required args
+  if [[ -z "$ACTION" ]]; then
+    die "$(color "Missing required argument: --action")"
+  fi
+}
+
+# ────────────────────────────────────────────────────────────────────────────────
+# Functions
+# ────────────────────────────────────────────────────────────────────────────────
+
+# ────────────────────────────────────────────────────────────────────────────────
+# Main logic
+# ────────────────────────────────────────────────────────────────────────────────
+
+parse_args "$@"
+
+echo "$(color "Performing") $($ACTION)..."
+
+case "$ACTION" in
+  install)
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+
+    echo "Helm installed"
+    ;;
+  uninstall)
+    sudo rm -rf "$(which helm)"
+    rm -rf ~/.helm
+    rm -rf ~/.cache/helm
+    rm -rf ~/.config/helm
+    rm -rf ~/.local/share/helm
+
+    
+    echo "Helm uninstalled"
+    ;;
+  *)
+    die "Unknown action: $ACTION"
+    ;;
+esac
