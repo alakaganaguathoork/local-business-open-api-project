eb41697437a9938ccec18f68a7762f4ce84d3885
Misha P <alakaganaguathoork@gmail.com>
2025-10-21 12:06:03 +0300

    Cleaning: add loki.yml pipeline

--- azure-pipelines/scripts/env/helm.sh
+++ azure-pipelines/scripts/env/helm.sh
@@ -69,7 +69,7 @@ echo "$(color "Performing") $($ACTION)..."
 
 case "$ACTION" in
   install)
-    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    curl -fsSL "https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3" -o get_helm.sh 
     chmod 700 get_helm.sh
     ./get_helm.sh
 
